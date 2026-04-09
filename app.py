091dc20599fd1c1e008de879747e7e7937530af8
Aryan Srivastava <181198778+ARYAN0000777@users.noreply.github.com>
2026-04-09 21:24:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -353,7 +353,7 @@ with tab1:
                         }
                         """
                         ai_resp  = ai_client.models.generate_content(
-                            model='gemini-2.5-pro-exp-03-25',
+                            model='gemini-2.5-flash-preview-04-17',
                             contents=[img, prompt]
                         )
                         raw_text = ai_resp.text.strip().replace("```json", "").replace("```", "").strip()
@@ -452,7 +452,7 @@ with tab2:
                     }
                     """
                     resp       = ai_client.models.generate_content(
-                        model='gemini-2.5-pro-exp-03-25',
+                        model='gemini-2.5-flash-preview-04-17',
                         contents=[types.Part.from_bytes(data=audio_value.getvalue(), mime_type='audio/wav'), audio_prompt]
                     )
                     clean_json = resp.text.strip().replace("```json", "").replace("```", "").strip()
